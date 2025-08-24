def9709e925f4916dd22fce9f071838fc539ab5e
nosyba <nosybamostafa2003@gmail.com>
2025-08-24 15:08:11 +0300

    code screen

--- oppo/src/App.js
+++ oppo/src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 //import Login from "./components/Login";
-import Forget from "./components/forget";
+//import Forget from "./components/forget";
+import ForgetPass from "./components/Forget-pass";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       
-      <Forget />
+      <ForgetPass />
   
     </div>
   );
