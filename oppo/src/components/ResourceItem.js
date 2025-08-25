13efa579c734427c48267053db76b8ba9c7c47ba
zeyad <s12218555@stu.najah.edu>
2025-08-25 13:33:17 +0300

    Implement resource management UI with add/edit modals

---
+++ oppo/src/components/ResourceItem.js
@@ -0,0 +1,29 @@
+
+import React from 'react';
+import { BsPencilSquare, BsTrash } from 'react-icons/bs';
+
+
+const ResourceItem = ({ resource, onEdit }) => {
+  return (
+    <div className="resource-item">
+      <div className="resource-info">
+        {resource.img ? (
+          <img src={resource.img} alt={resource.title} className="avatar-img" />
+        ) : (
+          <div className="resource-avatar" style={{ backgroundColor: resource.color }}>
+            {resource.initials}
+          </div>
+        )}
+        <span>{resource.title}</span>
+      </div>
+      <div className="resource-type">{resource.type}</div>
+      <div className="resource-actions">
+        
+        <BsPencilSquare className="action-edit" onClick={onEdit} />
+        <BsTrash className="action-delete" />
+      </div>
+    </div>
+  );
+};
+
+export default ResourceItem;
