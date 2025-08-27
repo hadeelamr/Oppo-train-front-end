98ac9496ef8a8c44dc1131195c9b2ce7f57eab94
rahafhamayel <116808826+rahafhamayel@users.noreply.github.com>
2025-08-27 22:40:09 +0300

    update

--- oppo/src/App.js
+++ oppo/src/App.js
@@ -1,25 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import MembersTable from "./components/MembersTable";
 import Sidebar from "./components/Sidebar";
 import Events from './pages/EventsPage';
 import Resources from './pages/Resources/ResourcesManagement';
-
+import Login from './components/Login'; 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <Router>
-      <div className="d-flex">
-        <Sidebar />
+      <Routes>
+       <Route path="/" element={<Login />} />
+      </Routes>
 
-        <div className="flex-grow-1 p-4">
-          <Routes>
-            <Route path="/" element={<MembersTable />} />
-            <Route path="/events" element={<Events />} />
-            <Route path="/resources" element={<Resources />} />
-          </Routes>
+      {isLoggedIn && (
+        <div className="d-flex">
+          <Sidebar />
+          <div className="flex-grow-1 p-4">
+            <Routes>
+              <Route path="/members" element={<MembersTable />} />
+              <Route path="/events" element={<Events />} />
+              <Route path="/resources" element={<Resources />} />
+            </Routes>
+          </div>
         </div>
-      </div>
+      )}
     </Router>
   );
 }
